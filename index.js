e0b23260951c761d43378b36abd17b36f16ab590
diemperdiem <38697401+diemperdiem@users.noreply.github.com>
2020-02-21 08:43:07 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const interestRate = 0.3;
+//No se peude reasignar el valor con const
+
+let name = 'Gabriel'; //Strin Literal
+let age = 30; //Number Literal
+let isApproved = true; //Bool Literal
+let firstName = undefined; //Not common but possible
+let SelectedColor = null; //to clear
+
+typeof name
+name = 1
+typeof name
+                //las variables pueden cambiar de tipo (Dynamic Language)
+
+
+console.log(name);
